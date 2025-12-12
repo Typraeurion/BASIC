Typraeurion/BASIC

commit c81af684c37b1b957fb5eadad238baa2a3c5ecc9

* Modified the grammar to allow NEXT to be followed by   a list of variables or no variable at all to support   compatibility with older BASIC variants which did this. * Patched the example program Checkers to check   for coordinates that are out of range. * Added two more example programs from BASIC   Computer Games: Hammurabi and Hangman.

run.c
@@ -648,17 +648,16 @@ cmd_for (struct statement_header *stmt)
     }
 }
 
-void
-cmd_next (struct statement_header *stmt)
+/* Evaluate NEXT for a given variable (by variable index).
+ * Returns 1 if we looped back to the statement following the FOR,
+ * 0 if we dropped out of the loop, or -1 if we encountered an error. */
+int
+evaluate_next (const int var)
 {
   struct statement_header *for_stmt;
   unsigned short *tp;
   double to_value, step_value;
-  int i, var;
-
-  /* Get the variable index.  Note that the order of tokens is:
-   * NEXT: IDENTIFIER, name index */
-  var = stmt->tokens[1];
+  int i;
 
   /* Search the FOR...NEXT stack for the variable. */
   for (i = for_stack_size - 1; i >= 0; --i)
@@ -668,9 +667,9 @@ cmd_next (struct statement_header *stmt)
     }
   if (i < 0)
     {
-      printf ("ERROR - NEXT: NOT IN %s LOOP", name_table[var]->contents);
+      printf ("ERROR - NEXT: NOT IN %s LOOP\n", name_table[var]->contents);
       executing = 0;
-      return;
+      return -1;
     }
   if (i < for_stack_size - 1)
     {
@@ -683,11 +682,11 @@ cmd_next (struct statement_header *stmt)
 				  for_stack[for_stack_size-1].for_statement);
   if (for_stmt != for_stack[for_stack_size-1].for_stmt)
     {
-      printf ("ERROR - NEXT: LOST FOR %s", name_table[var]->contents);
+      printf ("ERROR - NEXT: LOST FOR %s\n", name_table[var]->contents);
       /* Pop the FOR...NEXT stack */
       --for_stack_size;
       executing = 0;
-      return;
+      return -1;
     }
 
   /* Evaluate the TO end of the statement */
@@ -716,9 +715,76 @@ cmd_next (struct statement_header *stmt)
       /* Return to the statement following the FOR statement */
       current_line = for_stack[for_stack_size-1].for_line;
       current_statement = for_stack[for_stack_size-1].for_statement + 1;
+      return 1;
     } else {
       /* The loop has finished.  Pop the FOR...NEXT stack. */
       --for_stack_size;
+      return 0;
+    }
+}
+
+void
+cmd_next (struct statement_header *stmt)
+{
+  unsigned short *tp;
+  struct list_header *list;
+  struct list_item *next_var;
+  int i, var, status;
+
+  tp = &stmt->tokens[0];
+  if (*tp != ITEMLIST)
+    {
+      fputs ("NEXT: Unrecognized token - ", stderr);
+      list_token (tp, stderr);
+      fputc ('\n', stderr);
+      return;
+    }
+  list = (struct list_header *) (++tp);
+
+  if (for_stack_size <= 0)
+    {
+      puts ("ERROR - NEXT: NOT IN ANY LOOP\n");
+      executing = 0;
+      return;
+    }
+
+  /* For compatibility, the variable is optional.
+   * If omitted, use the top entry of the FOR stack. */
+  if (list->num_items == 0)
+    {
+      var = for_stack[for_stack_size-1].var_index;
+      evaluate_next (var);
+      return;
+    }
+
+  next_var = &list->item[0];
+  for (i = 0; i < list->num_items; i++)
+    {
+      tp = &next_var->tokens[0];
+      /* Get the variable index, which should follow an IDENTIFIER token. */
+      if (*tp != IDENTIFIER)
+	{
+	  fputs ("NEXT: Unrecognized token - ", stderr);
+	  list_token (tp, stderr);
+	  fputc ('\n', stderr);
+	  return;
+	}
+      var = tp[1];
+      status = evaluate_next (var);
+      if (status != 0)
+	/* Either looping to FOR or an error finishes this loop */
+	return;
+      /* Otherwise, the current variable is finished and we
+       * need to evaluate the next one in the list, if any. */
+      tp = (unsigned short *) &((char *) next_var)[next_var->length];
+      if ((i + 1 < list->num_items) && (*tp != ','))
+	{
+	  fputs ("NEXT: Unrecognized token - ", stderr);
+	  list_token (tp, stderr);
+	  fputc ('\n', stderr);
+	  return;
+	}
+      next_var = (struct list_item *) (++tp);
     }
 }
 

basic.y
@@ -436,7 +436,7 @@ statement: assignment
 	  /* Allocate a new statement */
 	  $<tokens>$ = add_tokens ("t", NEW);
 	}
-    | NEXT simplenumvar /* Step a variable; transfer control to `FOR' */
+    | NEXT simplenumvarlist /* Step a variable; transfer control to `FOR' */
 	{
 	  if (tracing & TRACE_GRAMMAR)
 	    fprintf (stderr,
@@ -676,6 +676,34 @@ varlist: anyvariable
     | varlist ',' anyvariable
     {
       /* Enlarge the array to include the next variable and comma. */
+      $<tokens>$ = add_tokens
+	("*tt#", $<tokens>1, ',',
+	 sizeof (struct list_item) + $<tokens>3[0] - sizeof (short),
+	 $<tokens>3);
+      ((struct list_header *) &$<tokens>$[2])->length
+	= $<tokens>$[0] - 2 * sizeof (short);
+      ((struct list_header *) &$<tokens>$[2])->num_items++;
+    }
+    ;
+
+/* Optional list of variables that may be used following a NEXT statement */
+simplenumvarlist: /* empty */
+    {
+      $<tokens>$ = add_tokens ("ttt", ITEMLIST, 2 * sizeof (short), 0);
+    }
+    | simplenumvar
+    {
+      /* Make the variable into a new token list. */
+      $<tokens>$ = add_tokens
+	("tttt*", ITEMLIST, 0, 1,
+	 sizeof (struct list_item) + $<tokens>1[0] - sizeof (short),
+	 $<tokens>1);
+      ((struct list_header *) &$<tokens>$[2])->length
+	= $<tokens>$[0] - 2 * sizeof (short);
+    }
+    | simplenumvarlist ',' simplenumvar
+    {
+      /* Enlarge the list to include the next variable and comma. */
       $<tokens>$ = add_tokens
 	("*tt#", $<tokens>1, ',',
 	 sizeof (struct list_item) + $<tokens>3[0] - sizeof (short),

docs/basic.texinfo
@@ -221,11 +221,11 @@ you.
 
 This implementation of @sc{basic} is case-insensitive.  You can enter
 statements in either uppercase or lowercase, or a mixture of both, and
-@sc{basic} will be able to recognize them.  When you list your program,
-all words that @sc{basic} knows will automatically be converted to
-uppercase.  The same applies to variable names (@xref{Variables}),
-except that variable names retain the same case the had when you first
-used them.
+@sc{basic} will be able to recognize them.  When you list your
+program, all words that @sc{basic} knows will automatically be
+converted to uppercase.  The same applies to variable names
+(@xref{Variables}), except that variable names retain the same case
+they had when you first used them.
 
 The exception to this is strings (@xref{Data Types}.) @ @sc{Basic}
 itself doesn't care what the contents of a string are, but when you
@@ -281,9 +281,13 @@ integers less than 4 billion will show all digits.)
 
 @cindex string
 
-Strings are entered into programs by enclosing them in double quotation
-marks, @samp{"LIKE THIS"}.  A string can contain any regular character
-except another double quotation mark or @key{Return}.
+Strings are entered into programs by enclosing them in double
+quotation marks, @samp{"LIKE THIS"}.  A string can contain any regular
+character except another double quotation mark.  Strings @emph{can}
+contain @key{Return} (newline) characters; some older programs relied
+on this capability to print multiple lines with a single statement.
+For readability and portability, you should not rely on this behavior;
+use a separate @code{PRINT} statement for each line instead.
 
 @node Exiting, Expressions, Data Types, General Syntax
 @section Exiting Out of @sc{Basic}
@@ -1466,6 +1470,23 @@ And the next example will show multiples of @math{\pi}:
 60 NEXT I
 @end example
 @end cartouche
+
+Some older variants of @sc{basic} allowed you to list multiple
+variables in the same @code{NEXT} statement.  For compatibility this
+@sc{basic} also allows it, so for example
+
+@cartouche
+@example
+50 NEXT J,I
+@end example
+@end cartouche
+
+is equivalent to the two separate @code{NEXT} statements in the
+previous example.  Some variants also allowed a @code{NEXT} statement
+@emph{without} specifying a variable; this would repeat the most
+recent (innermost) @code{NEXT} loop.  This implementation of
+@sc{basic} supports the bare @code{NEXT} syntax as well, but it
+renders the code less readable and is discouraged.
 
 In the event that the initial value of a FOR loop exceeds the TO value
 (with a positive STEP, or if it is less than the TO value with a

examples/Checkers.patches
@@ -1,3 +1,12 @@
-200 NEXT Y:NEXT X
-330 IF S(X,Y)=-2 THEN FOR A=-1 TO 1 STEP 2:FOR B=-1 TO 1 STEP 2:GOSUB 650:NEXT B:NEXT A
-350 NEXT Y:NEXT X:GOTO 1140
+1590 INPUT "FROM";E,H
+1600 IF E>=0 AND E<=7 AND H>=0 AND H<=7 THEN 1630
+1610 PRINT "PLEASE ENTER TWO COORDINATES FROM 0,0 THROUGH 7,7."
+1620 GOTO 1590
+1630 X=E:Y=H:IF S(X,Y)<=0 THEN 1590
+1670 INPUT "TO";A,B
+1672 IF A>=0 AND A<=7 AND B>=0 AND B<=7 THEN 1678
+1674 PRINT "PLEASE ENTER TWO COORDINATES FROM 0,0 THROUGH 7,7."
+1676 GOTO 1670
+1678 X=A:Y=B
+1690 PRINT CHR$(7);"NOT A VALID MOVE!":GOTO 1590
+1803 IF A1<0 OR A1>7 OR B1<0 OR B1>7 THEN PRINT "PLEASE ENTER TWO COORDINATES FROM 0,0 THROUGH 7,7.":GOTO 1802

examples/Hammurabi.BASIC
@@ -1,0 +1,119 @@
+10 PRINT TAB(32);"HAMURABI"
+20 PRINT TAB(15);"CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
+30 PRINT:PRINT:PRINT
+80 PRINT "TRY YOUR HAND AT GOVERNING ANCIENT SUMERIA"
+90 PRINT "FOR A TEN-YEAR TERM OF OFFICE.":PRINT
+95 D1=0: P1=0
+100 Z=0: P=95:S=2800: H=3000: E=H-S
+110 Y=3: A=H/Y: I=5: Q=1
+210 D=0
+215 PRINT:PRINT:PRINT "HAMURABI:  I BEG TO REPORT TO YOU,": Z=Z+1
+217 PRINT "IN YEAR";Z;",";D;"PEOPLE STARVED,";I;"CAME TO THE CITY,"
+218 P=P+I
+227 IF Q>0 THEN 230
+228 P=INT(P/2)
+229 PRINT "A HORRIBLE PLAGUE STRUCK!  HALF THE PEOPLE DIED."
+230 PRINT "POPULATION IS NOW";P
+232 PRINT "THE CITY NOW OWNS ";A;"ACRES."
+235 PRINT "YOU HARVESTED";Y;"BUSHELS PER ACRE."
+250 PRINT "THE RATS ATE";E;"BUSHELS."
+260 PRINT "YOU NOW HAVE ";S;"BUSHELS IN STORE.": PRINT
+270 IF Z=11 THEN 860
+310 C=INT(10*RND(1)): Y=C+17
+312 PRINT "LAND IS TRADING AT";Y;"BUSHELS PER ACRE."
+320 PRINT "HOW MANY ACRES DO YOU WISH TO BUY";
+321 INPUT Q: IF Q<0 THEN 850
+322 IF Y*Q<=S THEN 330
+323 GOSUB 710
+324 GOTO 320
+330 IF Q=0 THEN 340
+331 A=A+Q: S=S-Y*Q: C=0
+334 GOTO 400
+340 PRINT "HOW MANY ACRES DO YOU WISH TO SELL";
+341 INPUT Q: IF Q<0 THEN 850
+342 IF Q<A THEN 350
+343 GOSUB 720
+344 GOTO 340
+350 A=A-Q: S=S+Y*Q: C=0
+400 PRINT
+410 PRINT "HOW MANY BUSHELS DO YOU WISH TO FEED YOUR PEOPLE";
+411 INPUT Q
+412 IF Q<0 THEN 850
+418 REM *** TRYING TO USE MORE GRAIN THAN IS IN SILOS?
+420 IF Q<=S THEN 430
+421 GOSUB 710
+422 GOTO 410
+430 S=S-Q: C=1: PRINT
+440 PRINT "HOW MANY ACRES DO YOU WISH TO PLANT WITH SEED";
+441 INPUT D: IF D=0 THEN 511
+442 IF D<0 THEN 850
+444 REM *** TRYING TO PLANT MORE ACRES THAN YOU OWN?
+445 IF D<=A THEN 450
+446 GOSUB 720
+447 GOTO 440
+449 REM *** ENOUGH GRAIN FOR SEED?
+450 IF INT(D/2)<=S THEN 455
+452 GOSUB 710
+453 GOTO 440
+454 REM *** ENOUGH PEOPLE TO TEND THE CROPS?
+455 IF D<10*P THEN 510
+460 PRINT "BUT YOU HAVE ONLY";P;"PEOPLE TO TEND THE FIELDS!  NOW THEN,"
+470 GOTO 440
+510 S=S-INT(D/2)
+511 GOSUB 800
+512 REM *** A BOUNTIFUL HARVEST!
+515 Y=C: H=D*Y: E=0
+521 GOSUB 800
+522 IF INT(C/2)<>C/2 THEN 530
+523 REM *** RATS ARE RUNNING WILD!!
+525 E=INT(S/C)
+530 S=S-E+H
+531 GOSUB 800
+532 REM *** LET'S HAVE SOME BABIES
+533 I=INT(C*(20*A+S)/P/100+1)
+539 REM *** HOW MANY PEOPLE HAD FULL TUMMIES?
+540 C=INT(Q/20)
+541 REM *** HORROS, A 15% CHANCE OF PLAGUE
+542 Q=INT(10*(2*RND(1)-.3))
+550 IF P<C THEN 210
+551 REM *** STARVE ENOUGH FOR IMPEACHMENT?
+552 D=P-C: IF D>.45*P THEN 560
+553 P1=((Z-1)*P1+D*100/P)/Z
+555 P=C: D1=D1+D: GOTO 215
+560 PRINT: PRINT "YOU STARVED";D;"PEOPLE IN ONE YEAR!!!"
+565 PRINT "DUE TO THIS EXTREME MISMANAGEMENT YOU HAVE NOT ONLY"
+566 PRINT "BEEN IMPEACHED AND THROWN OUT OF OFFICE BUT YOU HAVE"
+567 PRINT "ALSO BEEN DECLARED NATIONAL FINK!!!!": GOTO 990
+710 PRINT "HAMURABI:  THINK AGAIN.  YOU HAVE ONLY"
+711 PRINT S;"BUSHELS OF GRAIN.  NOW THEN,"
+712 RETURN
+720 PRINT "HAMURABI:  THINK AGAIN.  YOU OWN ONLY";A;"ACRES.  NOW THEN,"
+730 RETURN
+800 C=INT(RND(1)*5)+1
+801 RETURN
+850 PRINT: PRINT "HAMURABI:  I CANNOT DO WHAT YOU WISH."
+855 PRINT "GET YOURSELF ANOTHER STEWARD!!!!!"
+857 GOTO 990
+860 PRINT "IN YOUR 10-YEAR TERM OF OFFICE,";P1;"PERCENT OF THE"
+862 PRINT "POPULATION STARVED PER YEAR ON THE AVERAGE, I.E. A TOTAL OF"
+865 PRINT D1;"PEOPLE DIED!!": L=A/P
+870 PRINT "YOU STARTED WITH 10 ACRES PER PERSON AND ENDED WITH"
+875 PRINT L;"ACRES PER PERSON.": PRINT
+880 IF P1>33 THEN 565
+885 IF L<7 THEN 565
+890 IF P1>10 THEN 940
+892 IF L<9 THEN 940
+895 IF P1>3 THEN 960
+896 IF L<10 THEN 960
+900 PRINT "A FANTASTIC PERFORMANCE!!!  CHARLEMANGE, DISRAELI, AND"
+905 PRINT "JEFFERSON COMBINED COULD NOT HAVE DONE BETTER!":GOTO 990
+940 PRINT "YOUR HEAVY-HANDED PERFORMANCE SMACKS OF NERO AND IVAN IV."
+945 PRINT "THE PEOPLE (REMAINING) FIND YOU AN UNPLEASANT RULER, AND,"
+950 PRINT "FRANKLY, HATE YOUR GUTS!!":GOTO 990
+960 PRINT "YOUR PERFORMANCE COULD HAVE BEEN SOMEWHAT BETTER, BUT"
+965 PRINT "REALLY WASN'T TOO BAD AT ALL. ";INT(P*.8*RND(1));"PEOPLE"
+970 PRINT "WOULD DEARLY LIKE TO SEE YOU ASSASSINATED BUT WE ALL HAVE OUR"
+975 PRINT "TRIVIAL PROBLEMS."
+990 PRINT: FOR N=1 TO 10: PRINT CHR$(7);: NEXT N
+995 PRINT "SO LONG FOR NOW.": PRINT
+999 END

examples/Hangman.BASIC
@@ -1,0 +1,81 @@
+10 PRINT TAB(32);"HANGMAN"
+20 PRINT TAB(15);"CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
+25 PRINT:PRINT:PRINT
+30 DIM P$(12,12),L$(20),D$(20),N$(26),U(50)
+40 C=1: N=50
+50 FOR I=1 TO 20: D$(I)="-": NEXT I: M=0
+60 FOR I=1 TO 26: N$(I)="": NEXT I
+70 FOR I=1 TO 12: FOR J=1 TO 12: P$(I,J)=" ": NEXT J: NEXT I
+80 FOR I=1 TO 12: P$(I,1)="X": NEXT I
+90 FOR I=1 TO 7: P$(1,I)="X": NEXT: P$(2,7)="X"
+95 IF C<N THEN 100
+97 PRINT "YOU DID ALL THE WORDS!!": STOP
+100 Q=INT(N*RND(1))+1
+110 IF U(Q)=1 THEN 100
+115 U(Q)=1: C=C+1: RESTORE: T1=0
+150 FOR I=1 TO Q: READ A$: NEXT I
+160 L=LEN(A$): FOR I=1 TO LEN(A$): L$(I)=MID$(A$,I,1): NEXT I
+170 PRINT "HERE ARE THE LETTERS YOU USED:"
+180 FOR I=1 TO 26: PRINT N$(I);: IF N$(I+1)="" THEN 200
+190 PRINT ",";: NEXT I
+200 PRINT: PRINT: FOR I=1 TO L: PRINT D$(I);: NEXT I: PRINT: PRINT
+210 INPUT "WHAT IS YOUR GUESS";G$: R=0
+220 FOR I=1 TO 26: IF N$(I)="" THEN 250
+230 IF G$=N$(I) THEN PRINT "YOU GUESSED THAT LETTER BEFORE!": GOTO 170
+240 NEXT I: PRINT "PROGRAM ERROR.  RUN AGAIN.": STOP
+250 N$(I)=G$: T1=T1+1
+260 FOR I=1 TO L: IF L$(I)=G$ THEN 280
+270 NEXT I: IF R=0 THEN 290
+275 GOTO 300
+280 D$(I)=G$: R=R+1: GOTO 270
+290 M=M+1: GOTO 400
+300 FOR I=1 TO L: IF D$(I)="-" THEN 320
+310 NEXT I: GOTO 390
+320 PRINT: FOR I=1 TO L: PRINT D$(I);: NEXT I: PRINT: PRINT
+330 INPUT "WHAT IS YOUR GUESS FOR THE WORD";B$
+340 IF A$=B$ THEN 360
+350 PRINT "WRONG.  TRY ANOTHER LETTER.": PRINT: GOTO 170
+360 PRINT "RIGHT!!  IT TOOK YOU";T1;"GUESSES!"
+370 INPUT "WANT ANOTHER WORD";W$: IF W$="YES" THEN 50
+380 PRINT: PRINT "IT'S BEEN FUN!  BYE FOR NOW.": GOTO 999
+390 PRINT "YOU FOUND THE WORD!": GOTO 370
+400 PRINT: PRINT: PRINT"SORRY, THAT LETTER ISN'T IN THE WORD."
+410 ON M GOTO 415,420,425,430,435,440,445,450,455,460
+415 PRINT "FIRST, WE DRAW A HEAD": GOTO 470
+420 PRINT "NOW WE DRAW A BODY.": GOTO 470
+425 PRINT "NEXT WE DRAW AN ARM.": GOTO 470
+430 PRINT "THIS TIME IT'S THE OTHER ARM.": GOTO 470
+435 PRINT "NOW, LET'S DRAW THE RIGHT LEG.": GOTO 470
+440 PRINT "THIS TIME WE DRAW THE LEFT LEG.": GOTO 470
+445 PRINT "NOW WE PUT UP A HAND.": GOTO 470
+450 PRINT "NEXT THE OTHER HAND.": GOTO 470
+455 PRINT "NOW WE DRAW ONE FOOT": GOTO 470
+460 PRINT "HERE'S THE OTHER FOOT -- YOU'RE HUNG!!"
+470 ON M GOTO 480,490,500,510,520,530,540,550,560,570
+480 P$(3,6)="-": P$(3,7)="-": P$(3,8)="-": P$(4,5)="(": P$(4,6)="."
+481 P$(4,8)=".":P$(4,9)=")":P$(5,6)="-":P$(5,7)="-":P$(5,8)="-":GOTO 580
+490 FOR I=6 TO 9: P$(I,7)="X": NEXT I: GOTO 580
+500 FOR I=4 TO 7: P$(I,I-1)="\": NEXT I: GOTO 580
+510 P$(4,11)="/": P$(5,10)="/": P$(6,9)="/": P$(7,8)="/": GOTO 580
+520 P$(10,6)="/": P$(11,5)="/": GOTO 580
+530 P$(10,8)="\": P$(11,9)="\": GOTO 580
+540 P$(3,11)="\": GOTO 580
+550 P$(3,3)="/": GOTO 580
+560 P$(12,10)="\": P$(12,11)="-": GOTO 580
+570 P$(12,3)="-": P$(12,4)="/"
+580 FOR I=1 TO 12: FOR J=1 TO 12: PRINT P$(I,J);: NEXT J
+590 PRINT: NEXT I: PRINT: PRINT: IF M<>10 THEN 170
+600 PRINT "SORRY, YOU LOSE.  THE WORD WAS ";A$
+610 PRINT "YOU MISSED THAT ONE.  DO YOU ";: GOTO 370
+620 INPUT "TYPE YES OR NO";Y$: IF Y$="YES" THEN 50
+700 DATA "GUM","SIN","FOR","CRY","LUG","BYE","FLY"
+710 DATA "UGLY","EACH","FROM","WORK","TALK","WITH","SELF"
+720 DATA "PIZZA","THING","FEIGN","FIEND","ELBOW","FAULT","DIRTY"
+730 DATA "BUDGET","SPIRIT","QUAINT","MAIDEN","ESCORT","PICKAX"
+740 DATA "EXAMPLE","TENSION","QUININE","KIDNEY","REPLICA","SLEEPER"
+750 DATA "TRIANGLE","KANGAROO","MAHOGANY","SERGEANT","SEQUENCE"
+760 DATA "MOUSTACHE","DANGEROUS","SCIENTIST","DIFFERENT","QUIESCENT"
+770 DATA "MAGISTRATE","ERRONEOUSLY","LOUDSPEAKER","PHYTOTOXIC"
+780 DATA "MATRIMONIAL","PARASYMPATHOMIMETIC","THIGMOTROPISM"
+990 PRINT "BYE NOW"
+999 END

commit ccf9165f679db3ecc54df3b9232deeb2a6daacb9

* Fixed a bug introduced in c81af68 where the NEXT following a bypassed   FOR loop was not evaluated correctly using the new variable list. * Added another example program from BASIC Computer Games: Horserace.

examples/Horserace.BASIC
@@ -1,0 +1,130 @@
+100 PRINT TAB(31);"HORSERACE"
+110 PRINT TAB(15);"CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY"
+120 PRINT:PRINT:PRINT
+210 DIM S(8)
+220 PRINT "WELCOME TO SOUTH PORTLAND HIGH RACETRACK"
+230 PRINT "                      ...OWNED BY LAURIE CHEVALIER"
+240 PRINT "DO YOU WANT DIRECTIONS";
+250 INPUT X$
+260 IF X$="NO" THEN320
+270 PRINT"UP TO 10 MAY PLAY.  A TABLE OF ODDS WILL BE PRINTED.  YOU"
+280 PRINT"MAY BET ANY + AMOUNT UNDER 100000 ON ONE HORSE."
+290 PRINT "DURING THE RACE, A HORSE WILL BE SHOWN BY ITS"
+300 PRINT"NUMBER.  THE HORSES RACE DOWN THE PAPER!"
+310 PRINT
+320 PRINT "HOW MANY WANT TO BET";
+330 INPUT C
+340 PRINT "WHEN ? APPEARS,TYPE NAME"
+350 FOR A=1 TO C
+360 INPUT W$(A)
+370 NEXT A
+380 PRINT
+390 PRINT"HORSE",,"NUMBER","ODDS"
+400 PRINT
+410 FOR I=1 TO 8: S(I)=0: NEXT I
+420 LET R=0
+430 FOR A=1 TO 8
+440 LET D(A)=INT(10*RND(1)+1)
+450 NEXT A
+460 FOR A=1 TO 8
+470 LET R=R+D(A)
+480 NEXT A
+490 LET V$(1)="JOE MAW"
+500 LET V$(2)="L.B.J."
+510 LET V$(3)="MR.WASHBURN"
+520 LET V$(4)="MISS KAREN"
+530 LET V$(5)="JOLLY"
+540 LET V$(6)="HORSE"
+550 LET V$(7)="JELLY DO NOT"
+560 LET V$(8)="MIDNIGHT"
+570 FOR N=1 TO8
+580 PRINT V$(N),,N,R/D(N);":1"
+590 NEXT N
+600 PRINT"--------------------------------------------------"
+610 PRINT "PLACE YOUR BETS...HORSE # THEN AMOUNT"
+620 FOR J=1 TO C
+630 PRINT W$(J);
+640 INPUT Q(J),P(J)
+650 IF P(J)<1 THEN 670
+660 IF P(J)<100000 THEN 690
+670 PRINT"  YOU CAN'T DO THAT!"
+680 GOTO 630
+690 NEXT J
+700 PRINT
+710 PRINT"1 2 3 4 5 6 7 8"
+720 PRINT"XXXXSTARTXXXX"
+730 FOR I=1 TON
+740 LET M=I
+750 LET M(I)=M
+760 LET Y(M(I))=INT(100*RND(1)+1)
+770 IF Y(M(I))<10 THEN860
+780 LET S=INT(R/D(I)+.5)
+790 IF Y(M(I))<S+17 THEN 880
+800 IF Y(M(I))<S+37 THEN 900
+810 IF Y(M(I))<S+57 THEN920
+820 IF Y(M(I))<77+S THEN940
+830 IF Y(M(I))<S+92 THEN960
+840 LET Y(M(I))=7
+850 GOTO 970
+860 LET Y(M(I))=1
+870 GOTO 970
+880 LET Y(M(I))=2
+890 GOTO 970
+900 LET Y(M(I))=3
+910 GOTO 970
+920 LET Y(M(I))=4
+930 GOTO 970
+940 LET Y(M(I))=5
+950 GOTO 970
+960 LET Y(M(I))=6
+970 NEXT I
+980 LET M=I
+990 FOR I=1 TO8
+1000LET S(M(I))=S(M(I))+Y(M(I))
+1010 NEXTI
+1020 LET I=1
+1030 FOR L=1 TO8
+1040 FORI=1TO 8-L
+1050 IF S(M(I))<S(M(I+1))THEN 1090
+1060 LET H=M(I)
+1070 LET M(I)=M(I+1)
+1080 LET M(I+1)=H
+1090 NEXT I
+1100 NEXT L
+1110 LET T=S(M(8))
+1120FOR I=1 TO8
+1130 LET B=S(M(I))-S(M(I-1))
+1140 IF B=0 THEN 1190
+1150 FOR A=1 TO B
+1160 PRINT
+1170IF S(M(I))>27 THEN1240
+1180 NEXT A
+1190 PRINT M(I);
+1200 NEXT I
+1210 FOR A=1 TO 28-T
+1220 PRINT
+1230 NEXT A
+1240 PRINT "XXXXFINISHXXXX";
+1242 PRINT
+1243 PRINT
+1244 PRINT "---------------------------------------------"
+1245 PRINT
+1250 IF T<28 THEN 720
+1270 PRINT "THE RACE RESULTS ARE:"
+1272 LET Z9=1
+1280 FOR I=8 TO 1STEP-1
+1290 LET F=M(I)
+1300 PRINT
+1310 PRINT Z9;"PLACE HORSE NO.";F,"AT ";R/D(F);":1"
+1312 LET Z9=Z9+1
+1320  NEXT I
+1330 FOR J=1 TO C
+1340 IF Q(J)<>M(8) THEN 1370
+1350LET N=Q(J)
+1355 PRINT
+1360 PRINT W$(J);" WINS $";(R/D(N))*P(J)
+1370 NEXT J
+1372 PRINT "DO YOU WANT TO BET ON THE NEXT RACE?"
+1374 INPUT "YES OR NO"; O$
+1376 IF O$="YES" THEN 380
+1380 END

examples/Horserace.patches
@@ -1,0 +1,14 @@
+260 IF X$="NO" THEN 320
+570 FOR N=1 TO 8
+580 PRINT V$(N);TAB(16);N,R/D(N);":1"
+730 FOR I=1 TO 8
+770 IF Y(M(I))<10 THEN 860
+810 IF Y(M(I))<S+57 THEN 920
+820 IF Y(M(I))<77+S THEN 940
+830 IF Y(M(I))<S+92 THEN 960
+990 FOR I=1 TO 8
+1010 NEXT I
+1030 FOR L=1 TO 8
+1040 FOR I=1 TO 8-L
+1120 FOR I=1 TO 8
+1170 IF S(M(I))>27 THEN 1240

run.c
@@ -545,13 +545,18 @@ cmd_if (struct statement_header *stmt)
   current_statement = 0;
 }
 
+// Defined later on down
+int evaluate_next (const int var);
+
 void
 cmd_for (struct statement_header *stmt)
 {
   unsigned short *tp;
-  int var;
+  int i, var, status;
   double to_value, step_value;
   struct line_header *line;
+  struct list_header *var_list;
+  struct list_item *var_item;
 
   /* Get the variable index.  Note that the order of tokens is:
    * FOR: NUMLVAL, IDENTIFIER, name index, '=', NUMEXPR, ...
@@ -644,12 +649,151 @@ cmd_for (struct statement_header *stmt)
       if (stmt->command != NEXT)
 	continue;
       /* Found a NEXT; see if the variable matches this FOR */
-      var = stmt->tokens[1];
-      if (var != for_stack[for_stack_size-1].var_index)
-	continue;
-      /* Pop the FOR...NEXT stack and continue with the next statement. */
-      --for_stack_size;
+      if (stmt->tokens[0] != ITEMLIST)
+	{
+	  fprintf (stderr, "NEXT (line %d): Unexpected token - ", current_line);
+	  tp = &stmt->tokens[0];
+	  list_token (tp, stderr);
+	  fputc ('\n', stderr);
+	  // Skip this statement
+	  continue;
+	}
+      var_list = (struct list_header *) &stmt->tokens[1];
+      if (var_list->num_items == 0)
+	{
+	  /* Without an explicit variable there's no way to tell
+	   * definitively whether it belongs to our current FOR;
+	   * just assume it does.  Pop our newest FOR...NEXT
+	   * stack item and continue with the next statement. */
+	  --for_stack_size;
+	  return;
+	}
+      var_item = &var_list->item[0];
+      for (i = 0; i < var_list->num_items; i++)
+	{
+	  tp = &var_item->tokens[0];
+	  if (*tp != IDENTIFIER)
+	    goto unrecognized_var_token_1;
+	  var = var_item->tokens[1];
+	  if (var == for_stack[for_stack_size-1].var_index)
+	    /* We have a match. */
+	    break;
+	  // No match; move on to the next (if any)
+	  tp = (unsigned short *) &((char *) var_item)[var_item->length];
+	  if ((i + 1 < var_list->num_items) && (*tp != ','))
+	    {
+	    unrecognized_var_token_1:
+	      fprintf (stderr, "NEXT (line %d): Unrecognized token - ",
+		       current_line);
+	      list_token (tp, stderr);
+	      fputc ('\n', stderr);
+	      // Skip this statement (break the current loop; continue the outer)
+	      goto skip_statement;
+	    }
+	  var_item = (struct list_item *) (++tp);
+	}
+      /* If we scanned all variables in this NEXT statement, keep looking. */
+      if (i < var_list->num_items)
+	break;
+    skip_statement:
+      continue;
+    }
+  /* Pop the FOR...NEXT stack. */
+  --for_stack_size;
+  /* If we have any more identifiers in this NEXT statement,
+   * evaluate the next one(s) until we loop or run out. */
+  for (i++ ; i < var_list->num_items; i++)
+    {
+      tp = (unsigned short *) &((char *) var_item)[var_item->length];
+      if ((i + 1 < var_list->num_items) && (*tp != ','))
+	{
+	  fprintf (stderr, "NEXT (line %d): Unrecognized token - ",
+		   current_line);
+	  list_token (tp, stderr);
+	  fputc ('\n', stderr);
+	  return;
+	}
+      var_item = (struct list_item *) (struct list_item *) (++tp);
+      if (var_item->tokens[0] != IDENTIFIER)
+	{
+	  fprintf (stderr, "NEXT (line %d): Unrecognized token - ",
+		   current_line);
+	  list_token (tp, stderr);
+	  fputc ('\n', stderr);
+	  return;
+	}
+      var = var_item->tokens[1];
+      status = evaluate_next (var);
+      if (status != 0)
+	/* Gone to the next loop or next statement on an error */
+	return;
+    }
+  /* At this point we ran out of NEXT variables;
+   * we'll just continue to the next statement. */
+}
+
+void
+cmd_next (struct statement_header *stmt)
+{
+  unsigned short *tp;
+  struct list_header *list;
+  struct list_item *next_var;
+  int i, var, status;
+
+  tp = &stmt->tokens[0];
+  if (*tp != ITEMLIST)
+    {
+      fputs ("NEXT: Unrecognized token - ", stderr);
+      list_token (tp, stderr);
+      fputc ('\n', stderr);
       return;
+    }
+  list = (struct list_header *) (++tp);
+
+  if (for_stack_size <= 0)
+    {
+      puts ("ERROR - NEXT: NOT IN ANY LOOP\n");
+      executing = 0;
+      return;
+    }
+
+  /* For compatibility, the variable is optional.
+   * If omitted, use the top entry of the FOR stack. */
+  if (list->num_items == 0)
+    {
+      var = for_stack[for_stack_size-1].var_index;
+      evaluate_next (var);
+      return;
+    }
+
+  next_var = &list->item[0];
+  for (i = 0; i < list->num_items; i++)
+    {
+      tp = &next_var->tokens[0];
+      /* Get the variable index, which should follow an IDENTIFIER token. */
+      if (*tp != IDENTIFIER)
+	{
+	  fputs ("NEXT: Unrecognized token - ", stderr);
+	  list_token (tp, stderr);
+	  fputc ('\n', stderr);
+	  return;
+	}
+      var = tp[1];
+      status = evaluate_next (var);
+      if (status != 0)
+	/* Either looping to FOR or an error finishes this loop */
+	return;
+      /* Otherwise, the current variable is finished and we
+       * need to evaluate the next one in the list, if any. */
+      tp = (unsigned short *) &((char *) next_var)[next_var->length];
+      if ((i + 1 < list->num_items) && (*tp != ','))
+	{
+	  fputs ("NEXT: Unrecognized token - ", stderr);
+	  list_token (tp, stderr);
+	  fputc ('\n', stderr);
+	  return;
+	}
+      next_var = (struct list_item *) (++tp);
     }
 }
 
@@ -725,71 +869,6 @@ evaluate_next (const int var)
       /* The loop has finished.  Pop the FOR...NEXT stack. */
       --for_stack_size;
       return 0;
-    }
-}
-
-void
-cmd_next (struct statement_header *stmt)
-{
-  unsigned short *tp;
-  struct list_header *list;
-  struct list_item *next_var;
-  int i, var, status;
-
-  tp = &stmt->tokens[0];
-  if (*tp != ITEMLIST)
-    {
-      fputs ("NEXT: Unrecognized token - ", stderr);
-      list_token (tp, stderr);
-      fputc ('\n', stderr);
-      return;
-    }
-  list = (struct list_header *) (++tp);
-
-  if (for_stack_size <= 0)
-    {
-      puts ("ERROR - NEXT: NOT IN ANY LOOP\n");
-      executing = 0;
-      return;
-    }
-
-  /* For compatibility, the variable is optional.
-   * If omitted, use the top entry of the FOR stack. */
-  if (list->num_items == 0)
-    {
-      var = for_stack[for_stack_size-1].var_index;
-      evaluate_next (var);
-      return;
-    }
-
-  next_var = &list->item[0];
-  for (i = 0; i < list->num_items; i++)
-    {
-      tp = &next_var->tokens[0];
-      /* Get the variable index, which should follow an IDENTIFIER token. */
-      if (*tp != IDENTIFIER)
-	{
-	  fputs ("NEXT: Unrecognized token - ", stderr);
-	  list_token (tp, stderr);
-	  fputc ('\n', stderr);
-	  return;
-	}
-      var = tp[1];
-      status = evaluate_next (var);
-      if (status != 0)
-	/* Either looping to FOR or an error finishes this loop */
-	return;
-      /* Otherwise, the current variable is finished and we
-       * need to evaluate the next one in the list, if any. */
-      tp = (unsigned short *) &((char *) next_var)[next_var->length];
-      if ((i + 1 < list->num_items) && (*tp != ','))
-	{
-	  fputs ("NEXT: Unrecognized token - ", stderr);
-	  list_token (tp, stderr);
-	  fputc ('\n', stderr);
-	  return;
-	}
-      next_var = (struct list_item *) (++tp);
     }
 }
 